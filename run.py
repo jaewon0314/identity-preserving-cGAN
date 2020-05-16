481f505ae33ad99c8a615dbf359cece6a7de4b3c
jaewon0314 <jaewonchoi0314@gmail.com>
2020-05-17 01:40:03 +0900

    commit

--- run.py
+++ run.py
@@ -256,15 +256,32 @@ def save_rgb_img(y_label,imgs, path):
     fig = plt.figure()
 
     for i,img in enumerate(imgs):
-        ax = fig.add_subplot(4, 4, i+1)
+        ax = fig.add_subplot(5, 5, i+1)
         ax.imshow((img+1.)/2.)
         ax.axis("off")
-        ax.set_title(''.join(str(x)for x in y_label[i]),fontsize= 1)
+        ax.set_title(''.join(str(x)for x in y_label[i]),fontsize= 3)
     if not os.path.isdir("../results"):
         os.makedirs("../results")
     plt.savefig(path)
     plt.close()
 
+
+def save_opt_img(real_imgs,gen_imgs,path):
+
+    fig = plt.figure()
+
+    for i in range(4):
+        ax1 = fig.add_subplot(4,2,2*i)
+        ax1.imshow(real_imgs[i])
+        ax1.axis("off")
+        ax2 = fig.add_subplot(4,2,2*i+1)
+        ax2.imshow((gen_imgs[i]+1.)/2.)
+        ax2.axis("off")
+
+    plt.savefig(path)
+    plt.close()
+
+    
 if __name__ == '__main__':
     # Define hyperparameters
     data_dir = "../img_align_celeba"
@@ -272,8 +289,8 @@ if __name__ == '__main__':
     batch_size = 128
     image_shape = (64, 64, 3)
     z_shape = 100
-    TRAIN_GAN = True
-    TRAIN_ENCODER = False
+    TRAIN_GAN = False
+    TRAIN_ENCODER = True
     TRAIN_GAN_WITH_FR = False
     fr_image_shape = (192, 192, 3)
 
@@ -325,20 +342,17 @@ if __name__ == '__main__':
         for epoch in range(epochs):
             print("Epoch:{}".format(epoch))
 
-            gen_losses = []
-            dis_losses = []
-
             number_of_batches = int(len(images) / batch_size)
             for index in tqdm(range(number_of_batches)):
                 if iter_time % 500 == 0 and iter_time > 0:
 
-                    inds = np.random.choice(202600, batch_size,replace=False)
+                    inds = np.random.choice(202599, batch_size,replace=False)
                     y_batch = np.array([y[i] for i in inds])
                     z_noise = np.random.normal(0, 1, size=(batch_size, z_shape))
 
                     gen_images = generator.predict_on_batch([z_noise, y_batch])
 
-                    save_rgb_img(y_batch[:16],gen_images[:16], path="../results/img_{}.png".format(iter_time))
+                    save_rgb_img(y_batch[:25],gen_images[:25], path="../results/img_{}.png".format(iter_time))
                 if epoch==0:
                     images_batch = load_batch(images[index * batch_size:(index + 1) * batch_size])
                     loaded_images.extend(images_batch)
@@ -371,7 +385,7 @@ if __name__ == '__main__':
                 """
 
                 z_noise2 = np.random.normal(0, 1, size=(batch_size, z_shape))
-                y_label = y[np.random.choice(202600, batch_size, replace=False)]
+                y_label = y[np.random.choice(202599, batch_size, replace=False)]
 
                 g_loss = adversarial_model.train_on_batch([z_noise2, y_label], [1] * batch_size)
 
@@ -386,10 +400,9 @@ if __name__ == '__main__':
                 if not os.path.isdir("../gen"):
                     os.makedirs("../gen")
                     os.makedirs("../dis")
-                if(epoch%5==0){
+                if(epoch%5==0):
                     generator.save_weights("../gen/generator_{}.h5".format(epoch))
                     discriminator.save_weights("../dis/discriminator_{}.h5".format(epoch))
-                }
             except Exception as e:
                 print("Error:", e)
             
@@ -404,10 +417,10 @@ if __name__ == '__main__':
         iter_time=0
         encoder = build_encoder()
         encoder.compile(loss=euclidean_distance_loss, optimizer='adam')
-
+        tensorboard.set_model(encoder)
         # Load the generator network's weights
         try:
-            generator.load_weights("generator.h5")
+            generator.load_weights("generator_20.h5")
         except Exception as e:
             print("Error:", e)
 
@@ -416,8 +429,6 @@ if __name__ == '__main__':
 
         for epoch in range(epochs):
             print("Epoch:", epoch)
-
-            encoder_losses = []
 
             number_of_batches = int(z_i.shape[0] / batch_size)
             for index in tqdm(range(number_of_batches)):
@@ -430,7 +441,6 @@ if __name__ == '__main__':
 
                 # Train the encoder model
                 encoder_loss = encoder.train_on_batch(generated_images, z_batch)
-                # print("Encoder loss:", encoder_loss)
                 iter_time+=1
                 write_log(tensorboard, "encoder_loss", encoder_loss, iter_time)
 
@@ -448,7 +458,7 @@ if __name__ == '__main__':
         encoder.load_weights("encoder.h5")
 
         # Load the generator network
-        generator.load_weights("generator.h5")
+        generator.load_weights("generator_20.h5")
 
         image_resizer = build_image_resizer()
         image_resizer.compile(loss=['binary_crossentropy'], optimizer='adam')
@@ -478,7 +488,7 @@ if __name__ == '__main__':
 
         # Compile the model
         fr_adversarial_model.compile(loss=euclidean_distance_loss, optimizer=adversarial_optimizer)
-
+        
         iter_time=0
         for epoch in range(epochs):
             print("Epoch:", epoch)
@@ -489,20 +499,24 @@ if __name__ == '__main__':
             for index in tqdm(range(number_of_batches)):
                 # print("Batch:", index + 1)
                 if iter_time % 500 == 0:
-
-                    y_batch = y[0:batch_size]
+                    pick=np.random.choice(np.shape(loaded_images)[0], batch_size, replace=False)
+                    y_batch = y[pick]
+                    real_images = np.array([loaded_images[i] for i in pick])
                     z_noise = np.random.normal(0, 1, size=(batch_size, z_shape))
 
                     gen_images = generator.predict_on_batch([z_noise, y_batch])
-
-                    for i, img in enumerate(gen_images[:5]):
-                        save_rgb_img(img, path="../results/img_opt_{}.png".format(iter_time))
+                    save_opt_img(real_images[:4],gen_images[:4], path="../drive/My Drive/resultsult/5_16_1_image_opt/img_opt_{}.png".format(iter_time))
                 
-                images_batch = loaded_images[index * batch_size:(index + 1) * batch_size]
+                if epoch==0:
+                    images_batch = load_batch(images[index * batch_size:(index + 1) * batch_size])
+                    loaded_images.extend(images_batch)
+                    y_batch = y[index * batch_size:(index + 1) * batch_size]
+                else:
+                    pick = np.random.choice(loaded_images.shape[0], batch_size, replace=False)
+                    images_batch = loaded_images[pick]
+                    y_batch = y[pick]
                 images_batch = images_batch / 127.5 - 1.0
                 images_batch = images_batch.astype(np.float32)
-
-                y_batch = y[index * batch_size:(index + 1) * batch_size]
 
                 images_batch_resized = image_resizer.predict_on_batch(images_batch)
 
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 a=np.array([1,3,4])
-y=np.array([1,2,3,4,5,6,7,8])
-print(y[a])
+y=[1,2,3,4,5,6,7,8]
+print(np.shape(y))
